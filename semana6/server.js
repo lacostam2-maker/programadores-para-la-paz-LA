c908ac7bca7f6506b2bb21683f4a726747669fbd
lacostam2-maker <lacostam2@unicartagena.edu.co>
2026-06-10 19:05:30 -0500

    Update server.js

--- semana6/server.js
+++ semana6/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Este comando es clave: le enseña a tu servidor a leer formato JSON
 app.use(express.json());
 
+// Ruta 1: Registro de usuario (Pregunta 6 y 7)
 app.post('/registro', (req, res) => {
-
   const nombre = req.body.nombre;
   const mensaje = req.body.mensaje;
 
@@ -13,9 +14,21 @@ app.post('/registro', (req, res) => {
     nombre: nombre,
     mensaje: mensaje
   });
-
 });
 
+// Ruta 2: Simular reporte ciudadano (Pregunta 8)
+app.post('/incidencia', (req, res) => {
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+
+  res.json({
+    mensaje: "Incidencia registrada",
+    tipo: tipo,
+    descripcion: descripcion
+  });
+});
+
+// Arrancar el servidor en el puerto 3000
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
